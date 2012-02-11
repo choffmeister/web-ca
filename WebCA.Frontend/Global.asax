8c8d9d61e52412b78f72a39faa16b61d407f1d33
choffmeister <thekwasti@googlemail.com>
2012-02-11 10:35:03 +0100

    cleaned up code

--- WebCA.Frontend/Global.asax
+++ WebCA.Frontend/Global.asax
@@ -1,1 +1,2 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebCA.Frontend.MvcApplication" Language="C#" %>
+﻿<%@ Application CodeBehind="Global.asax.cs" Inherits="WebCA.Frontend.MvcApplication"
+    Language="C#" %>
--- WebCA.Frontend/Global.asax.cs
+++ WebCA.Frontend/Global.asax.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace WebCA.Frontend
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode,
     // visit http://go.microsoft.com/?LinkId=9394801
 
     public class MvcApplication : System.Web.HttpApplication
@@ -26,7 +22,6 @@ namespace WebCA.Frontend
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
