81c32d1856ebd353a0d522ac442f9783ba42f8ee
choffmeister <thekwasti@googlemail.com>
2012-02-11 10:38:31 +0100

    created base layout

--- WebCA.Frontend/Properties/AssemblyInfo.cs
+++ WebCA.Frontend/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.3.1033")]
+[assembly: AssemblyVersion("0.1.5.1037")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
